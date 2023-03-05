10272a61271909b5d3cf6ef67e8ae649236fb6b3
mustafasayilgan <mustafasayilgann@gmail.com>
2023-03-06 01:23:13 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const webpack = require('webpack'); //to access built-in plugins
+const path = require('path');
+
+const config = {
+  entry: './index.js',
+  output: {
+    filename: 'app.js',
+    libraryTarget: 'umd'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './index.html'})
+  ],
+  devServer: {
+    host: '0.0.0.0'
+  }
+};
+
+module.exports = config;
